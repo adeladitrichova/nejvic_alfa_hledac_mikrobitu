ec5699288ff305be5619ed0a58ffa968aec5e5ad
Adéla Ditrichová <91247571+adeladitrichova@users.noreply.github.com>
2021-12-16 11:38:29 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 distance = 0
+rez = -10
 radio.set_group(111)
 def on_received_number(receivedNumber):
+    dist_rez = distance + rez
     data = radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
     console.log(data)
-    led.plot_bar_graph(data, distance)
+    led.plot_bar_graph(data, dist_rez)
 radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let distance = 0
+let rez = -10
 radio.setGroup(111)
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    let dist_rez = distance + rez
     let data = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     console.log(data)
-    led.plotBarGraph(data, distance)
+    led.plotBarGraph(data, dist_rez)
 })
